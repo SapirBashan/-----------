4a662f3843428d58b1e67bd3a5b784f560c2666b
SapirBashan <sapirbashan1@gmail.com>
2024-07-21 13:44:42 +0300

    changes to view

---
+++ select (for view).sql
@@ -0,0 +1,7 @@
+-- Select all columns from Warehouse_Equipment_View
+SELECT * 
+FROM Warehouse_Equipment_View;
+
+-- Select all columns from Volunteers_Shifts_View
+SELECT * 
+FROM Volunteers_Shifts_View;
